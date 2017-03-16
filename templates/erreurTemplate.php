967efcc6793e65057c2a2da052ce2f70459cadab
JulieBlg <julie.boulangee@gmail.com>
2017-03-16 20:33:54 +0100

    Update erreurTemplate.php
    Template swag

--- templates/erreurTemplate.php
+++ templates/erreurTemplate.php
@@ -1,8 +1,18 @@
-<div class="container" id="erreur">
+<div class="error" id="content">
 	<h2>Erreur</h2>
-	<?php
-		//$inscErrorText = $this->getArg('inscErrorText');
-		if(isset($inscErrorText))
-		echo '<span class="error">' . $inscErrorText . '</span>';
-	?>
+	
+	<div id="error">
+	
+		<?php
+			//$inscErrorText = $this->getArg('inscErrorText');
+			if(isset($inscErrorText))
+			echo '<span class="error">' . $inscErrorText . '</span>';
+		?>		
+	
+	</div>
+	
+	<p>
+		<a href="index.php?action=viewConnexion">Retour</a>
+	</p>
+	
 </div>
