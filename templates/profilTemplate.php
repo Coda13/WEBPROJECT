ab219b8478c8273731328a9e2d3948a7f00400d5
JulieBlg <julie.boulangee@gmail.com>
2017-03-16 20:58:25 +0100

    Update profilTemplate.php

--- templates/profilTemplate.php
+++ templates/profilTemplate.php
@@ -1,10 +1,26 @@
-
-
 <div id="content">
 
 	<h2>Bienvenue sur ton profil !</h2>
 	
+	<div id="avatar">
+		<img src="../images/anonymous.png" alt="avatarAn" style="width:100px;height:100px">
 	</div>
+	
+	<div id="info_perso">
+		<h1>Tes infos </h1>
+		<p>Nom :</p> 
+		<p>Prénom : </p>
+		<p>Mail : </p>
+	</div>
+	
+	<div id="stats">
+		<h1>Tes stats</h1>
+		<p>Nombre de parties gagnées : </p>
+	</div>
+	
+	
+	
 </div>
+
 
 
