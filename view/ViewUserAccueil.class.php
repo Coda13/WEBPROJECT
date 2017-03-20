e1a086f01bacf638f3d4feb8247cef853b11f4a2
JulieBlg <julie.boulangee@gmail.com>
2017-03-20 18:12:36 +0100

    Update ViewUserAccueil.class.php

--- view/ViewUserAccueil.class.php
+++ view/ViewUserAccueil.class.php
@@ -4,6 +4,8 @@
  		public function render(){
 			$this->templateNames['head'] = 'head2';
 			$this->loadTemplate($this->templateNames['head'], $this->args); 
+			$this->templateNames['menu'] = 'menu';
+			$this->loadTemplate($this->templateNames['menu'], $this->args); 
 			$this->templateNames['content'] = 'userAccueil';
 			$this->loadTemplate($this->templateNames['content'], $this->args); 
 			$this->loadTemplate($this->templateNames['foot'], $this->args);
