c8beda1afff8304a5b9962dda33570fd603b5579
Coda13 <alexandre.codaccioni@minesdedouai.fr>
2017-03-20 11:26:02 +0100

    Update ViewUserAccueil.class.php

--- view/ViewUserAccueil.class.php
+++ view/ViewUserAccueil.class.php
@@ -2,6 +2,7 @@
 	class ViewUserAccueil extends View{
 		
  		public function render(){
+			$this->templateNames['head'] = 'head2';
 			$this->loadTemplate($this->templateNames['head'], $this->args); 
 			$this->templateNames['content'] = 'userAccueil';
 			$this->loadTemplate($this->templateNames['content'], $this->args); 
