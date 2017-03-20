96b8a73ef44b4aca34e15e1fddacbe9c94035acd
Coda13 <alexandre.codaccioni@minesdedouai.fr>
2017-03-20 11:22:59 +0100

    Update UserController.class.php

--- controller/UserController.class.php
+++ controller/UserController.class.php
@@ -3,11 +3,11 @@
 		
 		public function __construct(){
 			
-			session_start();
+			//session_start();//fait dans l'index
 			
 			$request = Request::getCurrentRequest();
 			parent::__construct($request);
-			$_SESSION['login']=$request->read('user');
+			//$_SESSION['login']=$request->read('user');//fait dans le anonymous
 			//echo $request->read('user');
 			
 			
