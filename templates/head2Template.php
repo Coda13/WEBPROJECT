f4a3c0b5af73939e68e6c50505ac83226b3b1634
Coda13 <alexandre.codaccioni@minesdedouai.fr>
2017-03-20 11:24:14 +0100

    Add files via upload

---
+++ templates/head2Template.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>6QuiPrend</title>
+		<link	rel="stylesheet"	href="css/style.css"	type="text/css"	media="screen"	title="style"	charset="utf-8"	/>
+		<link rel="shortcut icon" href="../images/logo.jpg"/>	
+	</head>
+	<body>
+		<div id="header">
+			<div id="blocks">
+				<div id="icon">
+					<a href="index.php?action=defaultAction"><img src="../images/Logo2.png" alt="Logo2" style="width:100px;height:100px;"></a>
+				</div>
+				
+				<div id="block2">
+					<div id="inscription">
+						<a href="index.php?action=viewInscription">Inscription</a>
+					</div>
+					<button id="connexion" type="button">
+						<a href="index.php?action=viewConnexion">YOYOYO</a>
+					</button>
+				</div>
+			</div>
+		</div>
