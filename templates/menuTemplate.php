cdf37af99ea0d8b5889e8b5893b6ec3a25ff35ab
JulieBlg <julie.boulangee@gmail.com>
2017-03-20 18:09:27 +0100

    Add files via upload

---
+++ templates/menuTemplate.php
@@ -0,0 +1,11 @@
+<div id="menu">
+
+
+	<ul>
+	  <li><a href="index.php?action=defaultAction">Accueil</a></li>
+	  <li><a href="#news">Créer une partie</a></li>
+	  <li><a href="#contact">Rejoindre une partie</a></li>
+	  <li><a href="#about">A-propos</a></li>
+	</ul>
+
+</div>
