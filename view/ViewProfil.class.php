ad3ff91e4457f89f12d754dd74802781cbc87b37
JulieBlg <julie.boulangee@gmail.com>
2017-03-20 18:13:29 +0100

    Update ViewProfil.class.php

--- view/ViewProfil.class.php
+++ view/ViewProfil.class.php
@@ -3,6 +3,8 @@
 		
  		public function render(){
 			$this->loadTemplate($this->templateNames['head'], $this->args); 
+			$this->templateNames['menu'] = 'menu';
+			$this->loadTemplate($this->templateNames['menu'], $this->args); 
 			$this->templateNames['content'] = 'profil';
 			$this->loadTemplate($this->templateNames['content'], $this->args); 
 			$this->loadTemplate($this->templateNames['foot'], $this->args);
