e56a1a8bc0a3e009b9f1d0cda006f9c838543e02
JulieBlg <julie.boulangee@gmail.com>
2017-03-21 17:01:41 +0100

    Update menuTemplate.php

--- templates/menuTemplate.php
+++ templates/menuTemplate.php
@@ -1,11 +1,10 @@
-
 <div id="menu">
 
 
 	<ul>
 	  <li><a href="index.php?action=defaultAction">Accueil</a></li>
-	  <li><a href="#news">Créer une partie</a></li>
-	  <li><a href="#contact">Rejoindre une partie</a></li>
+	  <li><a href="index.php?action=viewCreationPartie">Créer une partie</a></li>
+	  <li><a href="index.php?action=viewRejoindrePartie">Rejoindre une partie</a></li>
 	  <li><a href="index.php?action=viewRegles">Règles du jeu</a></li>
 	</ul>
 
