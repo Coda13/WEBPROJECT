3f1bed785d193cc836aad42afad7b2c34d704439
JulieBlg <julie.boulangee@gmail.com>
2017-03-21 23:19:36 +0100

    Update ViewUserModifierProfil.class.php

--- view/ViewUserModifierProfil.class.php
+++ view/ViewUserModifierProfil.class.php
@@ -2,7 +2,8 @@
 	class ViewUserModifierProfil extends View{
 		
  		public function render(){
-			$this->loadTemplate($this->templateNames['head'], $this->args); 
+			$this->templateNames['head2'] = 'head2';
+			$this->loadTemplate($this->templateNames['head2'], $this->args); 
 			$this->templateNames['menu'] = 'menu';
 			$this->loadTemplate($this->templateNames['menu'], $this->args); 
 			$this->templateNames['content'] = 'userModifierProfil';
