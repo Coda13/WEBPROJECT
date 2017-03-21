830710c2f0be3ee8170adb2583895638cc93e5f2
JulieBlg <julie.boulangee@gmail.com>
2017-03-21 17:00:26 +0100

    Add files via upload

---
+++ view/ViewCreationPartie.class.php
@@ -0,0 +1,14 @@
+<?php
+	class ViewCreationPartie extends View{
+		
+ 		public function render(){
+			$this->templateNames['head'] = 'head2';
+			$this->loadTemplate($this->templateNames['head'], $this->args); 
+			$this->templateNames['menu'] = 'menu';
+			$this->loadTemplate($this->templateNames['menu'], $this->args); 
+			$this->templateNames['content'] = 'creationPartie';
+			$this->loadTemplate($this->templateNames['content'], $this->args); 
+			$this->loadTemplate($this->templateNames['foot'], $this->args);
+		}
+		
+	}
