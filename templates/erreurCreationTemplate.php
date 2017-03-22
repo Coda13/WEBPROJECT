229ab82b78c88ccd47914cdb052c8dcabbd6a76a
JulieBlg <julie.boulangee@gmail.com>
2017-03-22 09:28:43 +0100

    Add files via upload

---
+++ templates/erreurCreationTemplate.php
@@ -0,0 +1,18 @@
+<div class="error" id="content">
+	<h2>Erreur</h2>
+	
+	<div id="error">
+	
+		<?php
+			//$inscErrorText = $this->getArg('inscErrorText');
+			if(isset($inscErrorText))
+			echo '<span class="error">' . $inscErrorText . '</span>';
+		?>		
+	
+	</div>
+	
+	<p>
+		<a href="index.php?action=viewCreationPartie">Retour</a>
+	</p>
+	
+</div>
