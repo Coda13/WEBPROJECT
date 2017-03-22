9279e8572c7bdb17dffeb51202f98dce01bcdc40
JulieBlg <julie.boulangee@gmail.com>
2017-03-22 10:35:32 +0100

    Update creationPartieTemplate.php

--- templates/creationPartieTemplate.php
+++ templates/creationPartieTemplate.php
@@ -6,6 +6,21 @@
 
 	<h2>Nouvelle partie</h2>
 	
+	<center>
+		<form action="index.php?action=creationPartie" method="post">
+			<table>
+				<tr>
+					<th>Nom de la partie :</th>
+					<td><input type="text" name="idPartie"/></td>
+				</tr>
+					<th />
+					<td><input type="submit" value="Créer la partie" /></td>
+				</tr>
+			</table>
+		</form>
+				
+	</center>
+	
 	
 	
 	
