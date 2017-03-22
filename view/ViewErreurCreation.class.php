96a4129fdcec6edcf03b924661a33eaa0e32cc71
JulieBlg <julie.boulangee@gmail.com>
2017-03-22 09:28:18 +0100

    Add files via upload

---
+++ view/ViewErreurCreation.class.php
@@ -0,0 +1,13 @@
+<?php 
+	class ViewErreurCreation extends View {
+
+		public function render(){
+			$this->templateNames['head2'] = 'head2';
+			$this->loadTemplate($this->templateNames['head2'], $this->args); 
+			$this->templateNames['content'] = 'erreur';
+			$this->loadTemplate($this->templateNames['content'], $this->args); 
+			$this->loadTemplate($this->templateNames['foot'], $this->args);
+		}
+			
+	}
+?>
