d99cb22dc1a003bb8fdf58f78c2f3fe573111dfd
JulieBlg <julie.boulangee@gmail.com>
2017-03-22 11:22:07 +0100

    Update ViewErreurCreation.class.php

--- view/ViewErreurCreation.class.php
+++ view/ViewErreurCreation.class.php
@@ -4,7 +4,7 @@
 		public function render(){
 			$this->templateNames['head2'] = 'head2';
 			$this->loadTemplate($this->templateNames['head2'], $this->args); 
-			$this->templateNames['content'] = 'erreur';
+			$this->templateNames['content'] = 'erreurCreation';
 			$this->loadTemplate($this->templateNames['content'], $this->args); 
 			$this->loadTemplate($this->templateNames['foot'], $this->args);
 		}
