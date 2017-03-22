d3904afe5384db7a3940f2bba679d96231ed8d0f
Coda13 <alexandre.codaccioni@minesdedouai.fr>
2017-03-22 11:43:08 +0100

    Update rejoindrePartieTemplate.php

--- templates/rejoindrePartieTemplate.php
+++ templates/rejoindrePartieTemplate.php
@@ -1,24 +1,5 @@
 <?php
 	$login = User::get_login();
-	
-	function listePartie($login){
-		
-		
-		$arrayUsers=User::getTableauUsers();
-		
-		
-		$countUsers = sizeof($arrayUsers);
-		
-		for($j=0;$j<$countUsers;$j++){
-			
-				$arrayParties= Partie::getIdPartie($arrayUsers[$j]);
-				$countParties= sizeof($arrayParties);
-				for($i=0;$i<$countParties;$i++){
-			
-				echo "<br>".$arrayUsers[$j]." a crée : ".$arrayParties[$i] ; //faire les liens pour cliquer vers la partie
-			}
-		}
-	}
 ?>
 <div id="contentUser">
 
