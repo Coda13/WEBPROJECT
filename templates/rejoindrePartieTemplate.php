82a1530c9c7fca5e9decd31ca08fa033b28d8273
Coda13 <alexandre.codaccioni@minesdedouai.fr>
2017-03-22 10:58:15 +0100

    Update rejoindrePartieTemplate.php

--- templates/rejoindrePartieTemplate.php
+++ templates/rejoindrePartieTemplate.php
@@ -1,13 +1,22 @@
 <?php
 	$login = User::get_login();
+	
+	function listePartie($login){
+		
+		$array= Partie::getIdPartie($login);
+		$count = sizeof($array);
+			
+		for($i=0;$i<$count;$i++){	
+			
+			echo $login." a crée : ".$array[$i]; //faire les liens pour cliquer vers la partie
+			
+		}
+	}
 ?>
-
 <div id="contentUser">
 
 	<h2>Choisis une partie, et let's go !</h2>
-	
-	
-	
+		<?php listePartie($login)?>
 	
 	
 </div>
