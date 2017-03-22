8d124fbad8c2c52e828b89fa1684c6f5bfffb8c4
JulieBlg <julie.boulangee@gmail.com>
2017-03-22 16:31:05 +0100

    Update rejoindrePartieTemplate.php

--- templates/rejoindrePartieTemplate.php
+++ templates/rejoindrePartieTemplate.php
@@ -1,10 +1,21 @@
 <?php
 	$login = User::get_login();
+	
 ?>
+
 <div id="contentUser">
 
 	<h2>Choisis une partie, et let's go !</h2>
-		<?php Partie::listePartie($login)?>
+		
+	<?php 
+	//print_r($allParties) 
+	
+	for ($i=0;$i<sizeof($allParties);$i++){
+		echo "<div id='cellule'>".$allParties[$i]->ID_PARTIE." créée par ".$allParties[$i]->LOGIN."</div>";
+	}
+	
+	
+	?>
 	
 	
 </div>
